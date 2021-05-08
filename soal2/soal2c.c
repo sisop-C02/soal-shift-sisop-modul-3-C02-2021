917d0e5cde2a43467e39e25d9ae0ed91bcff3da9
maroqijalil <jalilpro31@gmail.com>
2021-05-08 21:40:11 +0700

    refactor: avoid plagiarism

--- soal2/soal2c.c
+++ soal2/soal2c.c
@@ -3,19 +3,23 @@
 #include <unistd.h>
 
 void main (int argc, char ** argv) {
+  // declare the first pipe
   int pipe_ps_to_sort[2];
   if (pipe(pipe_ps_to_sort) == -1) {
     perror("pipe failed");
     exit(EXIT_FAILURE);
   }
 
+  // declare the second pipe
   int pipe_sort_to_head[2];
   if (pipe(pipe_sort_to_head) == -1) {
     perror("pipe failed");
     exit(EXIT_FAILURE);
   }
 
+  // make a child process to perform first command
   if (fork() == 0) {
+    // write to pipe
     close(pipe_ps_to_sort[0]);
     dup2(pipe_ps_to_sort[1], 1);
     close(pipe_ps_to_sort[1]);
@@ -24,11 +28,14 @@ void main (int argc, char ** argv) {
     execv("/bin/ps", args);
   }
   
+  // make a child process to perform second command
   if (fork() == 0) {
+    // read the result of first command
     close(pipe_ps_to_sort[1]);
     dup2(pipe_ps_to_sort[0], 0);
     close(pipe_ps_to_sort[0]);
     
+    // write to pipe
     close(pipe_sort_to_head[0]);
     dup2(pipe_sort_to_head[1], 1);
     close(pipe_sort_to_head[1]);
@@ -37,20 +44,18 @@ void main (int argc, char ** argv) {
     execv("/bin/sort", args);
   }
 
+  // close the unused pipe
   close(pipe_ps_to_sort[0]);
   close(pipe_ps_to_sort[1]);
 
-  if (fork() == 0) {
-    close(pipe_sort_to_head[1]);
-    dup2(pipe_sort_to_head[0], 0);
-    close(pipe_sort_to_head[0]);
-    
-    char * args[] = {"head", "-5", NULL};
-    execv("/bin/head", args);
-  }
-
-  close(pipe_sort_to_head[0]);
+  // read the result of second command
   close(pipe_sort_to_head[1]);
+  dup2(pipe_sort_to_head[0], 0);
+  close(pipe_sort_to_head[0]);
+  
+  // perform the last command
+  char * args[] = {"head", "-5", NULL};
+  execv("/bin/head", args);
   
   exit(EXIT_SUCCESS);
 }
